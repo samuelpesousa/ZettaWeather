c0e8069f867deb0ff6af74f4359dc4baad86ab53
Samuel de Paula e Sousa <samuelpsousa@hotmail.com>
2025-05-01 21:40:49 -0300

    Criação de Pastas Para o Projeto.

---
+++ src/components/Navbar/navbar.tsx
@@ -0,0 +1,6 @@
+const Navbar = () => (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark px-3">
+      <a className="navbar-brand" href="/">React API Viewer ZETTALAB</a>
+    </nav>
+  );
+  export default Navbar;
